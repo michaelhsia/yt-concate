5c2a3d0f53fe36aaf92938076391fe8d0858418e
Michael Hsia <michaelhsia8716@gmail.com>
2022-09-25 11:36:30 +0800

    Successfully download captions and save video list to file

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,7 +1,10 @@
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.pipeline.steps.preflight import Preflight
+from yt_concate.pipeline.steps.postflight import Postflight
 from yt_concate.pipeline.steps.step import StepException
-
 from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.utils import Utils
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
@@ -11,11 +14,14 @@ def main():
         'channel_id': CHANNEL_ID
     }
     steps = [
+        Preflight(),
         GetVideoList(),
+        DownloadCaptions(),
+        Postflight(),
     ]
-
+    utils = Utils()
     p = Pipeline(steps)
-    p.run(inputs)
+    p.run(inputs, utils)
 
 
 if __name__ == '__main__':
--- yt_concate/settings.py
+++ yt_concate/settings.py
@@ -4,3 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
+
+VIDEO
+DOWNLOADS_DIR = 'downloads'
+VIDEOS_DIR = os.path.join(DOWNLOADS_DIR + 'videos')
+CAPTIONS_DIR = os.path.join(DOWNLOADS_DIR + 'captions')
